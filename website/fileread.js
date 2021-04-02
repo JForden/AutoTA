0b395c1bd5885354dd46ef491f1b5a178b63184b
Jack Forden <jrfaster@gmail.com>
2021-04-01 20:05:05 -0500

    json

---
+++ website/fileread.js
@@ -0,0 +1,12 @@
+//var fs = require("fs"),obj
+var ErrNum=0;
+const linearray=[];
+var txt = '{"type": "convention","module": "realmidterm","obj": "","line": 7,"column": 16,"path": "realmidterm.py","symbol": "trailing-whitespace","message": "Trailing whitespace","message-id": "C0303"}'
+var data = JSON.parse(txt);
+push(document.getElementById("?") = data.line);
+
+for(var key in txt){
+    if(key==line){
+        linearray.push(txt[key])
+    }
+}
