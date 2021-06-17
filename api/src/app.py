0e064b2772680b7eda729b5ebcbb59d05ae704b4
Alex Gebhard <39505376+a1g3@users.noreply.github.com>
2021-06-17 00:44:40 -0500

    Remove zip

--- api/src/app.py
+++ api/src/app.py
@@ -11,7 +11,7 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config['UPLOAD_FOLDER'] = '/home/alex/Documents/Repositories/AutoTA/'
-    app.config['ALLOWED_EXTENSIONS'] = ['py', 'zip']
+    app.config['ALLOWED_EXTENSIONS'] = ['py']
     app.config['GRADE_ONDEMAND_SCRIPT_PATH'] = '/home/alex/Documents/Repositories/AutoTA/GradeOnD.sh'
     app.config["JWT_SECRET_KEY"] = "ob1L04WeQ1U0H5Kiybk9rMoQigVhoGJCKBxC6KxF85G89vAK3L903I073JXQ"
 
