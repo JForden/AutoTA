beb1ba164aa690433755515e9dcb93c7628c26a0
TimopheyKor <timopheykor@gmail.com>
2025-04-17 14:17:10 -0400

    initial thread form UI

--- ui/src/components/ForumPageComponent.tsx
+++ ui/src/components/ForumPageComponent.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import 'semantic-ui-css/semantic.min.css';
+import { Input, Button } from 'semantic-ui-react';
 
 interface ChatProps {
     class_id?: string;
@@ -9,10 +10,20 @@ interface ChatProps {
 
 const ForumPageComponent = () => {
     const [ClassName, setClassName] = useState<String>("");
+    const [thread, setThread] = useState<String>("");
+    const [threadBody, setThreadBody] = useState<String>("");
+
+    // handleSubmit creates a new thread on form submission, then resets the thread.
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        console.log({ thread });
+        console.log({ threadBody });
+        setThread("");
+        setThreadBody("");
+    };
 
     // Get the class ID from state params:
     let { class_id } = useParams<ChatProps>();
-    //var cid = class_id ? parseInt(class_id) : -1;
 
     // API call to get the class name from the current ID:
     useEffect(() => {
@@ -28,11 +39,40 @@ const ForumPageComponent = () => {
     })
 
     const title = `Class ${ClassName} Discussion Board`;
+    //TODO: Add description input as well as thread input
     return (
-        <div style={{ margin: "5%" }}>
-            <h1>{title}</h1>
-        </div>
-
+        <main style={{ margin: "5%" }}>
+            <div>
+                <h1>{title}</h1>
+            </div>
+            <div style={{ margin: "5%" }}>
+                <h2 className="center">Create a Thread</h2>
+                <form className="ui form" onSubmit={handleSubmit}>
+                    <div className="formInputContainer">
+                        <label htmlFor="thread">Thread Title</label>
+                        <Input
+                            fluid
+                            type="text"
+                            name="thread"
+                            label={{ icon: 'asterisk' }}
+                            labelPosition='right corner'
+                            placeholder='Is this a good placeholder question?'
+                            required
+                            value={thread}
+                            onChange={(e) => setThread(e.target.value)}
+                        />
+                        <label htmlFor="body">Additional Details</label>
+                        <textarea
+                            name="body"
+                            placeholder='Type details clarifying your question here!'
+                            value={threadBody as string}
+                            onChange={(e) => setThreadBody(e.target.value)}
+                        />
+                    </div>
+                    <button className="ui button">Create Thread</button>
+                </form>
+            </div>
+        </main>
     )
 }
 
